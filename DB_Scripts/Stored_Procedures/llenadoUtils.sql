bdce417b0e1797d1e4f6daa916bc622bee81876f
Joseph Vargas <Josephvb10@users.noreply.github.com>
2018-10-27 07:13:21 -0600

    Update llenadoUtils.sql

--- DB_Scripts/Stored_Procedures/llenadoUtils.sql
+++ DB_Scripts/Stored_Procedures/llenadoUtils.sql
@@ -1,7 +1,6 @@
 USE GREEN_TEC;  
 GO
 
-
 CREATE PROCEDURE EpecieRandom AS
   SELECT TOP 1 IdEspecie FROM GREEN_TEC.Especie ORDER BY newid()
 GO;
@@ -11,12 +10,12 @@ CREATE PROCEDURE VisitanteRandom AS
 GO;
   
 CREATE PROCEDURE VehiculosDeParqueNacional   
-    @ParqueNacional int
+  @ParqueNacional int
 AS
-    SELECT GREEN_TEC.Vehiculo.IdVehiculo FROM Green_TEC.Vehiculo
-		INNER JOIN GREEN_TEC.VehiculoXPersonal ON GREEN_TEC.VehiculoXPersonal.IdVehiculo = GREEN_TEC.Vehiculo.IdVehiculo
-		INNER JOIN GREEN_TEC.Personal ON GREEN_TEC.VehiculoXPersonal.IdPersonal = GREEN_TEC.Personal.IdPersonal
-		INNER JOIN GREEN_TEC.Area ON GREEN_TEC.Personal.IdAreaAsignada = GREEN_TEC.Area.IdArea
-		WHERE GREEN_TEC.Area.IdParqueNacional = @ParqueNacional
+  SELECT GREEN_TEC.Vehiculo.IdVehiculo FROM Green_TEC.Vehiculo
+  INNER JOIN GREEN_TEC.VehiculoXPersonal ON GREEN_TEC.VehiculoXPersonal.IdVehiculo = GREEN_TEC.Vehiculo.IdVehiculo
+  INNER JOIN GREEN_TEC.Personal ON GREEN_TEC.VehiculoXPersonal.IdPersonal = GREEN_TEC.Personal.IdPersonal
+  INNER JOIN GREEN_TEC.Area ON GREEN_TEC.Personal.IdAreaAsignada = GREEN_TEC.Area.IdArea
+  WHERE GREEN_TEC.Area.IdParqueNacional = @ParqueNacional
 GO;
 
